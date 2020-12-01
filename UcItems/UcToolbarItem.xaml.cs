198d8351afa5ea2f2329cfcf421c93681e9bb97c
Yuri Choi <yrchoi1126@gmail.com>
2020-12-02 01:28:29 +0900

    statusbar 작업

---
+++ UcItems/UcToolbarItem.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YuristNodepad.UcItems
+{
+    /// <summary>
+    /// UcToolbarItem.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UcToolbarItem : UserControl
+    {
+        private bool isOpen = false;
+        public UcToolbarItem()
+        {
+            InitializeComponent();
+
+            lbMenu.MouseLeftButtonUp += LbMenu_MouseLeftButtonUp;
+            if (isOpen)
+            {
+                lbMenuList.Visibility = Visibility.Visible;
+            }
+            else 
+            {
+                lbMenuList.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void LbMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isOpen = !isOpen;
+            
+        }
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -4,15 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace YuristNodepad.ViewModel
 {
-    public class MainViewModel:Observer
+    public class MainViewModel : Observer
     {
         private static MainViewModel _mainViewModel;
-        public static MainViewModel GetInstance() 
+        public static MainViewModel GetInstance()
         {
-            if (_mainViewModel == null) 
+            if (_mainViewModel == null)
             {
                 _mainViewModel = new MainViewModel();
             }
@@ -20,20 +21,72 @@ namespace YuristNodepad.ViewModel
         }
 
         private string richFullText;
+        private string richCursorLine;
+        private string richCursorCol;
+        private string richZoom;
+        private string richEncoding;
+        private RichTextBox rtb;
 
-        public string RichFullText 
+        public string RichFullText
         {
-            get 
+            get
             {
                 return richFullText;
             }
-            set 
+            set
             {
                 richFullText = value;
                 OnPropertyChanged("RichFullText");
             }
         }
+        public string RichCursorLine
+        {
+            get
+            {
+                return richCursorLine;
+            }
+            set
+            {
+                richCursorLine = value;
+                OnPropertyChanged("RichCursorLine");
+            }
+        }
+        public string RichCursorCol
+        {
+            get
+            {
+                return richCursorCol;
+            }
+            set
+            {
+                richCursorCol = value;
+                OnPropertyChanged("RichCursorCol");
+            }
+        }
+        public string RichZoom
+        {
+            get
+            {
+                return richZoom;
+            }
+            set
+            {
+                richZoom = value;
+                OnPropertyChanged("RichZoom");
+            }
+        }
+        public string RichEncoding
+        {
+            get
+            {
+                return richEncoding;
+            }
+            set
+            {
+                richEncoding = value;
+                OnPropertyChanged("RichEncoding");
+            }
+        }
 
-        
     }
 }
