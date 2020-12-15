db713365f4d0cf7543bcc1c24957b22a53c23248
yrchoi <yrchoi@trenbe.com>
2020-12-15 18:54:16 +0900

    Toolbar 구현

---
+++ Model/ToolbarMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuristNodepad.Model
+{
+    public class ToolbarMenu
+    {
+        public string Header { get; set; }
+        public List<string> Menu { get; set; }
+        public ToolbarMenu(string header, List<string> menu) 
+        {
+            Header = header;
+            Menu = menu;
+        }
+    }
+}
